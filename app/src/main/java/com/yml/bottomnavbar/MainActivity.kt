5cc705a26eb5471810b36a1f1c50562c7a14ef38
Deeksha K <deeksha1399@gmail.com>
2020-12-05 10:32:40 +0530

    Initial commit

---
+++ app/src/main/java/com/yml/bottomnavbar/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.yml.bottomnavbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_nav_bar)
+
+        bottomNavigationView.background = null
+        bottomNavigationView.menu.getItem(2).isEnabled = false
+
+        bottomNavigationView.itemIconTintList = null;
+
+    }
+}
